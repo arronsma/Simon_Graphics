f5f208dd827e130cbfe44a62b9836e5367f5ce22
No Name <arronsma@github.com>
2024-10-13 23:18:41 +0800

    stb and ebo

--- Renderer/Shader/vertexShader.vs
+++ Renderer/Shader/vertexShader.vs
@@ -1,6 +1,13 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aTexCoord;
+
+#uniform mat4 model;
+#uniform mat4 view;
+#uniform mat4 projection;
 
 void main () {
 	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1);
+	#gl_Position = projection * view * model * gl_Position;
 }
--- Renderer/src/Renderer.cpp
+++ Renderer/src/Renderer.cpp
@@ -1,4 +1,7 @@
 #include "Renderer.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 
 void processInput(GLFWwindow* window)
 {
@@ -49,15 +52,17 @@ int Renderer::Render() {
 	Shader shader("vertexShader.vs", "fragmentShader.fs");
 
 	float vertices[] = {
-		-0.25f, -0.25f, 0.0f,
-		 0.25f, -0.25f, 0.0f,
-		 0.0f,  0.25f, 0.0f,
-
-		 0.25f, -0.25f, 0.0f,
-		 0.75f, -0.25f, 0.0f,
-		 0.5f,  0.25f, 0.0f,
+		// positions          // colors           // texture coords
+		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
 	};
 
+	unsigned int indices[] = {  // note that we start from 0!
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
+	};
 	
 	unsigned VAO_ID;
 	glGenVertexArrays(1, &VAO_ID);
@@ -67,8 +72,19 @@ int Renderer::Render() {
 	glGenBuffers(1, &VBO_ID);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO_ID);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+	
+	glBindVertexArray(0);
+
+	unsigned EBO_ID;
+	glGenBuffers(1, &EBO_ID);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_ID);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// 渲染循环
 	while (!glfwWindowShouldClose(window))
@@ -79,7 +95,9 @@ int Renderer::Render() {
 		glBindVertexArray(VAO_ID);
 		processInput(window);
 		//glDrawBuffer(VBO_ID);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		//glDrawArrays(GL_TRIANGLES, 0, 4);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, indices);
+
 		// glfw: 交换缓冲区和轮询IO事件（按键按下/释放、鼠标移动等）
 		glfwSwapBuffers(window);
 		glfwPollEvents();
